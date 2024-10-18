53292f8989b5d90e43dd4d0a2c5e5c8b41be00e2
Roberto Menezes <129512414+RobertSDM@users.noreply.github.com>
2024-10-18 10:15:23 -0300

    Update genius_arduino.ino

--- genius_arduino.ino
+++ genius_arduino.ino
@@ -1,3 +1,5 @@
+#include <EEPROM.h>
+
 enum GameState
 {
   IDLE,
@@ -62,23 +64,20 @@ int btn_yellow_vl;
 int btn_green_vl;
 int tempoRespostaJogador = 300; // em milissegundos
 int tamanhoSequenciaJogo = 4;
-int nivelAtual = 1; 
+int nivelAtual = 1;
 int recordeAtual = 0;
 int maiorRecorde = 0;
 
 // Definicoes de jogo
 GameState state = IDLE;
 SequenciaLeds sequencia_idle = {
-  100,
-  {
-    {true, false, true, false},
-    {false, false, true, false},
-    {true, true, true, false},
-    {false, false, false, true}
-  },
+    100,
+    {{true, false, true, false},
+     {false, false, true, false},
+     {true, true, true, false},
+     {false, false, false, true}},
 };
 NivelDificuldade dificuldade = EASY;
-
 
 // Espera o jogador iniciar o jogo
 void idle();
@@ -87,12 +86,12 @@ bool verificaBotaoEspecificoClicou(Color cor);
 // Se algum dos botoes que for clicado retorna true
 Color verificaAlgumBotaoClicou();
 // Acende a cor enviada por parametro
-void onBtnClick(Color cor);
+void emitirSomBotao(Color cor);
 // Quando o iniciar o jogo inicia cria a sequencia
-void criarSequenciaJogo(Color* arr); // criarSequenciaJogo ou criarSequencia ?
-// Verifica se as entradas do usuario estao dentro 
+void criarSequenciaJogo(Color *arr); // criarSequenciaJogo ou criarSequencia ?
+// Verifica se as entradas do usuario estao dentro
 // da sequencia gerada
-bool verificaSequenciaJogador(Color* sequencia);
+bool verificaSequenciaJogador(Color *sequencia);
 // Espera determinado tempo para o jogador escolher uma cor
 Color pegarBotaoJogador();
 void salvarRecorde();
@@ -122,94 +121,105 @@ void loop()
 
   switch (state)
   {
-    case IDLE:
-      idle();
-      break;
-    case RUNNING:
-      criarSequenciaJogo(sequencia);
-      // resultado = verificaSequenciaJogador(sequencia);
+  case IDLE:
+    idle();
+    break;
+  case RUNNING:
+    nivelAtual++;
+    if(nivelAtual > recordeAtual)
+    {
+      recordeAtual = nivelAtual;
+    }
+    criarSequenciaJogo(sequencia);
+    resultado = verificaSequenciaJogador(sequencia);
 
-      // if(resultado == false)
-      // {
-      //   state = GAME_OVER;
-      // }
-
-      break;
-    case GAME_OVER:
-      //caso seja game over, o buzzer apitar e voltar pro estado de jogar - play again
-      digitalWrite(BUZZER, HIGH);
-      delay(1000); 
-      digitalWrite(BUZZER, LOW);
-      state = RESTART; 
-      break;
-    case RESTART:
-      // reinicia o jogo para o estado inicial
-      // ver alguma forma de mudar o nivel do jogo
-      tamanhoSequenciaJogo = 4; // Reset da sequência inicial
-      dificuldade = EASY; // Volta para a dificuldade inicial
-      state = IDLE; // Coloca o jogo de volta em "espera"
-      break;
-
+    if (resultado == false)
+    {
+      state = GAME_OVER;
+    }
+    
+    atualizarDificuldade();
+    break;
+  case GAME_OVER:
+    // caso seja game over, o buzzer apitar e voltar pro estado de jogar - play again
+    digitalWrite(BUZZER, HIGH);
+    delay(700);
+    digitalWrite(BUZZER, LOW);
+    salvarRecorde();
+    state = RESTART;
+    break;
+  case RESTART:
+    // reinicia o jogo para o estado inicial
+    // ver alguma forma de mudar o nivel do jogo
+    tamanhoSequenciaJogo = 4; // Reset da sequência inicial
+    nivelAtual = 1;
+    dificuldade = EASY;       // Volta para a dificuldade inicial
+    state = IDLE;             // Coloca o jogo de volta em "espera"
+    break;
   };
 }
 
 void idle()
 {
   
-  for(int i = 0; i < 4; i++)
+  for (int i = 0; i < 4; i++)
   {
-    if(verificaAlgumBotaoClicou()){
-        state = RUNNING;
-      
-        digitalWrite(LED_RED, LOW);
-        digitalWrite(LED_GREEN, LOW);
-        digitalWrite(LED_BLUE, LOW);
-        digitalWrite(LED_YELLOW, LOW);
-    	break;
+    if (verificaAlgumBotaoClicou())
+    {
+      state = RUNNING;
+
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, LOW);
+      digitalWrite(LED_YELLOW, LOW);
+      break;
     };
 
     digitalWrite(LED_RED, sequencia_idle.rgb[i].red);
-	delay(sequencia_idle.espera);
+    delay(sequencia_idle.espera);
 
     digitalWrite(LED_GREEN, sequencia_idle.rgb[i].green);
     delay(sequencia_idle.espera);
 
     digitalWrite(LED_BLUE, sequencia_idle.rgb[i].blue);
-	delay(sequencia_idle.espera);
-   
+    delay(sequencia_idle.espera);
+
     digitalWrite(LED_YELLOW, sequencia_idle.rgb[i].yellow);
     delay(sequencia_idle.espera);
   }
-  
 }
-
 
 Color verificaAlgumBotaoClicou()
 {
-    
+
   btn_red_vl = digitalRead(BTN_RED);
   btn_blue_vl = digitalRead(BTN_BLUE);
   btn_yellow_vl = digitalRead(BTN_YELLOW);
   btn_green_vl = digitalRead(BTN_GREEN);
-  
+
   if (btn_red_vl == LOW)
   {
+    emitirSomBotao(RED);
     return RED;
-  }else if(btn_blue_vl == LOW)
+  }
+  else if (btn_blue_vl == LOW)
   {
+    emitirSomBotao(BLUE);
     return BLUE;
-  }else if(btn_yellow_vl == LOW)
+  }
+  else if (btn_yellow_vl == LOW)
   {
+    emitirSomBotao(YELLOW);
     return YELLOW;
-  }else if(btn_green_vl == LOW)
+  }
+  else if (btn_green_vl == LOW)
   {
+    emitirSomBotao(GREEN);
     return GREEN;
   }
-  
+
   return NONE_COLOR;
 };
-
-
 
 bool verificaBotaoEspecificoClicou(Color cor)
 {
@@ -217,69 +227,76 @@ bool verificaBotaoEspecificoClicou(Color cor)
   btn_blue_vl = digitalRead(BTN_BLUE);
   btn_yellow_vl = digitalRead(BTN_YELLOW);
   btn_green_vl = digitalRead(BTN_GREEN);
-  
+
   if (btn_red_vl == LOW && RED == cor)
   {
-    return true;
-  }else if( btn_blue_vl == LOW && BLUE == cor)
-  {
-    return true;
-  }else if(btn_yellow_vl == LOW && YELLOW == cor)
-  {
-    return true;
-  }else if(btn_green_vl == LOW && GREEN == cor)
-  {
+    emitirSomBotao(RED);
     return true;
   }
-  
+  else if (btn_blue_vl == LOW && BLUE == cor)
+  {
+    emitirSomBotao(BLUE);
+    return true;
+  }
+  else if (btn_yellow_vl == LOW && YELLOW == cor)
+  {
+    emitirSomBotao(YELLOW);
+    return true;
+  }
+  else if (btn_green_vl == LOW && GREEN == cor)
+  {
+    emitirSomBotao(GREEN);
+    return true;
+  }
+
   return false;
-  
 };
 
 // definir o tamanho da sequencia no parametro e ir definindo/mudando de acordo com o nivel
-void criarSequenciaJogo(Color* arr){
+void criarSequenciaJogo(Color *arr)
+{
   Color cores[] = {RED, GREEN, YELLOW, BLUE};
-  
-  for (int i = 0; i < tamanhoSequenciaJogo; i++){
-  	// utiliza o random pra gerar numero aleatorio entre 0 e 3, as 4 cores possiveis e converte para Color 
+
+  for (int i = 0; i < tamanhoSequenciaJogo; i++)
+  {
+    // utiliza o random pra gerar numero aleatorio entre 0 e 3, as 4 cores possiveis e converte para Color
     arr[i] = cores[random(0, 4)];
   }
 }
 
-
-
-void onBtnClick(Color cor) {
-    // switch (cor) {
-    //     case RED:
-    //         tone(BUZZER, 262); // Nota C
-    //         break;
-    //     case GREEN:
-    //         tone(BUZZER, 294); // Nota D
-    //         break;
-    //     case BLUE:
-    //         tone(BUZZER, 330); // Nota E
-    //         break;
-    //     case YELLOW:
-    //         tone(BUZZER, 392); // Nota G
-    //         break;
-    // }
-    delay(200);
-    noTone(BUZZER);
+void emitirSomBotao(Color cor)
+{
+  switch (cor)
+  {
+  case RED:
+    tone(BUZZER, 262); // Nota C
+    break;
+  case GREEN:
+    tone(BUZZER, 294); // Nota D
+    break;
+  case BLUE:
+    tone(BUZZER, 330); // Nota E
+    break;
+  case YELLOW:
+    tone(BUZZER, 392); // Nota G
+    break;
+  }
+  delay(100);
+  noTone(BUZZER);
 }
 
-
-bool verificaSequenciaJogador(Color* sequencia)
+bool verificaSequenciaJogador(Color *sequencia)
 {
   Color entradaJogador;
-  for(int i = 0; i < tamanhoSequenciaJogo; i++)
+  for (int i = 0; i < tamanhoSequenciaJogo; i++)
   {
     entradaJogador = pegarBotaoJogador();
-    if(entradaJogador != NONE_COLOR && entradaJogador != sequencia[i])
+    if (entradaJogador != NONE_COLOR && entradaJogador != sequencia[i])
     {
       return false;
     }
   }
-  
+
   return true;
 }
 
@@ -287,44 +304,53 @@ Color pegarBotaoJogador()
 {
   Color entrada;
   int comeco = millis();
-  while(true)
+  while (true)
   {
     Color corClicada = verificaAlgumBotaoClicou();
-    if(corClicada != NONE_COLOR)
+    if (corClicada != NONE_COLOR)
     {
-	  return corClicada;
+      return corClicada;
     }
-    
-    if(millis() - comeco  > tempoRespostaJogador)
+
+    if (millis() - comeco > tempoRespostaJogador)
     {
       return NONE_COLOR;
     }
   }
 }
 
-void atualizarDificuldade() {
-  if (nivelAtual % 5 == 0) { // Aumenta a dificuldade a cada 5 níveis
-    if (dificuldade == EASY) dificuldade = MEDIUM;
-    else if (dificuldade == MEDIUM) dificuldade = HARD;
-    else if (dificuldade == HARD) dificuldade = VERY_HARD;
+void atualizarDificuldade()
+{
+  if (nivelAtual % 5 == 0)
+  { // Aumenta a dificuldade a cada 5 níveis
+    if (dificuldade == EASY)
+      dificuldade = MEDIUM;
+    else if (dificuldade == MEDIUM)
+      dificuldade = HARD;
+    else if (dificuldade == HARD)
+      dificuldade = VERY_HARD;
   }
 
-  if (nivelAtual % 3 == 0) { // Aumenta o tamanho da sequência a cada 3 níveis
+  if (nivelAtual % 3 == 0)
+  { // Aumenta o tamanho da sequência a cada 3 níveis
     tamanhoSequenciaJogo++;
   }
 }
 
-
-
-
-void salvarRecorde() {
-    if (recordeAtual > maiorRecorde) 
-    {
-        maiorRecorde = recordeAtual;  
-    }
-    Serial.print("Game Over! Seu recorde atual foi: ");
-    Serial.println(recordeAtual);
-    Serial.print("Maior recorde: ");
-    Serial.println(maiorRecorde);
+void salvarRecorde()
+{
+  if (recordeAtual > maiorRecorde)
+  {
+    maiorRecorde = recordeAtual;
+  }
+  
+  EEPROM.write(1, recordeAtual);
+  delay(10);
+  EEPROM.write(0, maiorRecorde);
+  delay(10);
+  
+  Serial.print("Game Over! Seu recorde atual foi: ");
+  Serial.println(recordeAtual);
+  Serial.print("Maior recorde: ");
+  Serial.println(maiorRecorde);
 }
-
