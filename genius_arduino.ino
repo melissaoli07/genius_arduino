0b2522b09471ae557232be7d6f3d155eccf3b2ef
Roberto Menezes <129512414+RobertSDM@users.noreply.github.com>
2024-11-01 10:11:54 -0300

    limpando a memória da forma correta

--- genius_arduino.ino
+++ genius_arduino.ino
@@ -1,19 +1,19 @@
 #include <EEPROM.h>
-
+ 
 enum GameState {
   IDLE,
   RUNNING,
   GAME_OVER,
   RESTART
 };
-
+ 
 enum NivelDificuldade {
   EASY,
   MEDIUM,
   HARD,
   VERY_HARD
 };
-
+ 
 // Todas as cores que podem ser escolhidas
 enum Color {
   RED,
@@ -22,7 +22,7 @@ enum Color {
   YELLOW,
   NONE_COLOR
 };
-
+ 
 // Estrutura para salvar quais cores estão ativas
 struct RGB {
   bool red;
@@ -30,28 +30,28 @@ struct RGB {
   bool blue;
   bool yellow;
 };
-
+ 
 struct SequenciaLeds {
   // tempo espera entre padroes de leds
   int espera;
   RGB rgb[4];
 };
-
+ 
 // Leds
 const int LED_RED = 21;
 const int LED_BLUE = 22;
 const int LED_YELLOW = 23;
 const int LED_GREEN = 25;
-
+ 
 // Buttons
 const int BTN_RED = 18;
 const int BTN_BLUE = 19;
 const int BTN_YELLOW = 26;
 const int BTN_GREEN = 27;
-
+ 
 // Buzzer
 const int BUZZER = 14;
-
+ 
 // Variaveis globais
 int localSalvarEEPROM;
 int btn_red_vl;
@@ -62,8 +62,8 @@ const int tempoExibirLed = 500;  // em milissegundos
 const int dificuldadeInicial = EASY;
 const int nivelInicial = 0;
 const int tamanhoSequenciaJogoInicial = 1;
-
-
+ 
+ 
 // Definicoes de jogo
 int clicando = false;
 int tamanhoSequenciaJogo = tamanhoSequenciaJogoInicial;
@@ -80,7 +80,7 @@ SequenciaLeds sequencia_idle = {
 };
 int dificuldade = dificuldadeInicial;
 Color* sequenciaCarregada = nullptr;
-
+ 
 // Espera o jogador iniciar o jogo
 void idle();
 // Se o botao especifico for clicado faca algo
@@ -99,24 +99,26 @@ Color pegarBotaoJogador();
 void salvarRecorde();
 void atualizarDificuldade();
 void exibirSequencia(Color* sequencia);
-
+void liberarMemoriaSequencia();
+void criarNovaSequencia();
+ 
 void setup() {
   pinMode(LED_RED, OUTPUT);
   pinMode(LED_BLUE, OUTPUT);
   pinMode(LED_YELLOW, OUTPUT);
   pinMode(LED_GREEN, OUTPUT);
-
+ 
   pinMode(BTN_RED, INPUT_PULLUP);
   pinMode(BTN_BLUE, INPUT_PULLUP);
   pinMode(BTN_YELLOW, INPUT_PULLUP);
   pinMode(BTN_GREEN, INPUT_PULLUP);
-
+ 
   pinMode(BUZZER, OUTPUT);
-
-
+ 
+ 
   Serial.begin(9600);
   randomSeed(analogRead(0));
-
+ 
   if(EEPROM.read(0) != 0xff)
   {
     localSalvarEEPROM = EEPROM.read(0);
@@ -125,13 +127,26 @@ void setup() {
     EEPROM.write(0, localSalvarEEPROM);
   }
 }
-
+ 
+// Libera a memória da sequência carregada
+void liberarMemoriaSequencia() {
+  if (sequenciaCarregada != nullptr) {
+    delete[] sequenciaCarregada;
+    sequenciaCarregada = nullptr;
+  }
+}
+ 
+// Cria a sequência com o tamanho atual
+void criarNovaSequencia() {
+  liberarMemoriaSequencia();  // Libera a memória da sequência anterior
+  sequenciaCarregada = new Color[tamanhoSequenciaJogo];  // Aloca nova sequência
+}
+ 
 void loop() {
-
-  bool resultado;
   Color novaCor;
-  Color sequenciaAtual[tamanhoSequenciaJogo];
-
+  Color* sequenciaAtual = nullptr;
+  bool resultado;
+ 
   switch (state) {
     case IDLE:
       idle();
@@ -139,36 +154,35 @@ void loop() {
     case RUNNING:
       nivelAtual++;
       novaCor = geraCorAleatoria();
-
+ 
+      // Cria a nova sequência e copia a antiga, se houver
+      criarNovaSequencia();
       if (sequenciaCarregada != nullptr) {
         for (int i = 0; i < tamanhoSequenciaJogo - 1; i++) {
-          sequenciaAtual[i] = sequenciaCarregada[i];
+          sequenciaCarregada[i] = sequenciaCarregada[i];
         }
       }
-
-      sequenciaAtual[tamanhoSequenciaJogo - 1] = novaCor;
-
-      exibirSequencia(sequenciaAtual);
-      resultado = verificaSequenciaJogador(sequenciaAtual);
-
-      if (resultado == false) {
+ 
+      sequenciaCarregada[tamanhoSequenciaJogo - 1] = novaCor;
+ 
+      exibirSequencia(sequenciaCarregada);
+      resultado = verificaSequenciaJogador(sequenciaCarregada);
+ 
+      if (!resultado) {
         nivelAtual--;
         state = GAME_OVER;
       }
-
+ 
       if (nivelAtual > recordeAtual) {
         recordeAtual = nivelAtual;
       }
-
-      sequenciaCarregada = sequenciaAtual;
+ 
       tamanhoSequenciaJogo++;
-
       atualizarDificuldade();
       delay(250);
       break;
     case GAME_OVER:
       noTone(BUZZER);
-      // caso seja game over, o buzzer apitar e voltar pro estado de jogar - play again
       tone(BUZZER, 262);
       delay(700);
       noTone(BUZZER);
@@ -176,102 +190,100 @@ void loop() {
       state = RESTART;
       break;
     case RESTART:
-      // reinicia o jogo para o estado inicial
-      // ver alguma forma de mudar o nivel do jogo
-      tamanhoSequenciaJogo = tamanhoSequenciaJogoInicial;  // Reset da sequência inicial
+      liberarMemoriaSequencia();  // Libera a memória quando reinicia o jogo
+      tamanhoSequenciaJogo = tamanhoSequenciaJogoInicial;
       nivelAtual = nivelInicial;
-      dificuldade = dificuldadeInicial;  // Volta para a dificuldade inicial
-      // sequenciaCarregada = nullptr;
-      state = IDLE;  // Coloca o jogo de volta em "espera"
+      dificuldade = dificuldadeInicial;
+      state = IDLE;
       break;
   };
 }
-
+ 
 void idle() {
   for (int i = 0; i < 6; i++) {
     if (verificaAlgumBotaoClicou() != NONE_COLOR) {
       state = RUNNING;
-
+ 
       digitalWrite(LED_RED, LOW);
       digitalWrite(LED_GREEN, LOW);
       digitalWrite(LED_BLUE, LOW);
       digitalWrite(LED_YELLOW, LOW);
-
+ 
       delay(500);
       break;
     };
-
+ 
     digitalWrite(LED_RED, sequencia_idle.rgb[i].red);
     delay(sequencia_idle.espera);
-
+ 
     if (verificaAlgumBotaoClicou() != NONE_COLOR) {
       state = RUNNING;
-
+ 
       digitalWrite(LED_RED, LOW);
       digitalWrite(LED_GREEN, LOW);
       digitalWrite(LED_BLUE, LOW);
       digitalWrite(LED_YELLOW, LOW);
-
+ 
       delay(500);
       break;
     };
-
+ 
     digitalWrite(LED_GREEN, sequencia_idle.rgb[i].green);
     delay(sequencia_idle.espera);
-
+ 
     if (verificaAlgumBotaoClicou() != NONE_COLOR) {
       state = RUNNING;
-
+ 
       digitalWrite(LED_RED, LOW);
       digitalWrite(LED_GREEN, LOW);
       digitalWrite(LED_BLUE, LOW);
       digitalWrite(LED_YELLOW, LOW);
-
+ 
       delay(500);
       break;
     };
-
+ 
     digitalWrite(LED_BLUE, sequencia_idle.rgb[i].blue);
     delay(sequencia_idle.espera);
-
+ 
     if (verificaAlgumBotaoClicou() != NONE_COLOR) {
       state = RUNNING;
-
+ 
       digitalWrite(LED_RED, LOW);
       digitalWrite(LED_GREEN, LOW);
       digitalWrite(LED_BLUE, LOW);
       digitalWrite(LED_YELLOW, LOW);
-
+ 
       delay(500);
       break;
     };
-
+ 
     digitalWrite(LED_YELLOW, sequencia_idle.rgb[i].yellow);
     delay(sequencia_idle.espera);
-
+ 
     if (verificaAlgumBotaoClicou() != NONE_COLOR) {
       state = RUNNING;
-
+ 
       digitalWrite(LED_RED, LOW);
       digitalWrite(LED_GREEN, LOW);
       digitalWrite(LED_BLUE, LOW);
       digitalWrite(LED_YELLOW, LOW);
-
+ 
       delay(500);
       break;
     };
   }
 }
-
+ 
 Color verificaAlgumBotaoClicou() {
-
+ 
   btn_red_vl = digitalRead(BTN_RED);
   btn_blue_vl = digitalRead(BTN_BLUE);
   btn_yellow_vl = digitalRead(BTN_YELLOW);
   btn_green_vl = digitalRead(BTN_GREEN);
-
+ 
   clicando = true;
-
+ 
   if (btn_red_vl == LOW) {
     emitirSomBotao(RED);
     return RED;
@@ -286,16 +298,16 @@ Color verificaAlgumBotaoClicou() {
     return GREEN;
   }
   clicando = false;
-
+ 
   return NONE_COLOR;
 };
-
+ 
 bool verificaBotaoEspecificoClicou(Color cor) {
   btn_red_vl = digitalRead(BTN_RED);
   btn_blue_vl = digitalRead(BTN_BLUE);
   btn_yellow_vl = digitalRead(BTN_YELLOW);
   btn_green_vl = digitalRead(BTN_GREEN);
-
+ 
   if (btn_red_vl == LOW && RED == cor) {
     emitirSomBotao(RED);
     return true;
@@ -309,19 +321,19 @@ bool verificaBotaoEspecificoClicou(Color cor) {
     emitirSomBotao(GREEN);
     return true;
   }
-
+ 
   return false;
 };
-
+ 
 // definir o tamanho da sequencia no parametro e ir definindo/mudando de acordo com o nivel
 Color geraCorAleatoria() {
   Color cores[] = { RED, GREEN, YELLOW, BLUE };
   // utiliza o random pra gerar numero aleatorio entre 0 e 3, as 4 cores possiveis e converte para Color
   Color corAleatoria = cores[random(0, 4)];
-
+ 
   return corAleatoria;
 }
-
+ 
 void emitirSomBotao(Color cor) {
   switch (cor) {
     case RED:
@@ -340,20 +352,20 @@ void emitirSomBotao(Color cor) {
   delay(100);
   noTone(BUZZER);
 }
-
+ 
 bool verificaSequenciaJogador(Color* sequencia) {
   Color entradaJogador;
   for (int i = 0; i < tamanhoSequenciaJogo; i++) {
     entradaJogador = pegarBotaoJogador();
-
+ 
     if (entradaJogador == NONE_COLOR || entradaJogador != sequencia[i]) {
       return false;
     }
   }
-
+ 
   return true;
 }
-
+ 
 Color pegarBotaoJogador() {
   while (true) {
     // Verifica se o botão está sendo clicado
@@ -372,7 +384,7 @@ Color pegarBotaoJogador() {
     }
   }
 }
-
+ 
 void atualizarDificuldade() {
   if (nivelAtual % 5 == 0) {  // Aumenta a dificuldade a cada 5 níveis
     if (dificuldade == EASY)
@@ -383,41 +395,41 @@ void atualizarDificuldade() {
       dificuldade = VERY_HARD;
   }
 }
-
+ 
 void salvarRecorde() {
   if (recordeAtual > maiorRecorde) {
     maiorRecorde = recordeAtual;
   }
-
+ 
   EEPROM.write(localSalvarEEPROM, maiorRecorde);
   delay(10);
   localSalvarEEPROM++;
   EEPROM.write(0, localSalvarEEPROM);
   delay(10);
-
+ 
   // Serial.print("Game Over! Seu recorde atual foi: ");
   // Serial.println(recordeAtual);
   // Serial.print("Maior recorde: ");
   // Serial.println(maiorRecorde);
 }
-
-
-
+ 
+ 
+ 
 void exibirSequencia(Color* sequencia) {
   int tempo = floor(tempoExibirLed / (dificuldade * 2 + 1));
-
+ 
   for (int i = 0; i < tamanhoSequenciaJogo; i++) {
     emitirSomBotao(sequencia[i]);
     if (sequencia[i] == RED) {
       digitalWrite(LED_RED, HIGH);
       delay(tempo);
       digitalWrite(LED_RED, LOW);
-
+ 
     } else if (sequencia[i] == BLUE) {
       digitalWrite(LED_BLUE, HIGH);
       delay(tempo);
       digitalWrite(LED_BLUE, LOW);
-
+ 
     } else if (sequencia[i] == GREEN) {
       digitalWrite(LED_GREEN, HIGH);
       delay(tempo);
@@ -429,3 +441,4 @@ void exibirSequencia(Color* sequencia) {
     }
   }
 }
+ 
